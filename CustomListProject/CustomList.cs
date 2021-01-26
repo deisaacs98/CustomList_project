0bf88832580974def744e257f43e4986615ef166
deisaacs98 <deisaacs98@gmail.com>
2021-01-26 07:59:22 -0600

    Adding methods in CustomList class.

--- CustomListProject/CustomList.cs
+++ CustomListProject/CustomList.cs
@@ -39,9 +39,21 @@ namespace CustomListProject
             //If the capacity is equal to the count, a new array must be created before adding the item.
             //This new array will have double the capacity of the previous array.
             //When an item is added, the index value should be equal to the count.
+            count++;
+
+            if (count == capacity)
+            {
+                capacity = 2 * capacity;
+                T[] temp = new T[capacity];
+
+            }
+            else if (count < capacity)
+            {
+                items[count] = item;
+            }
 
         }
-
+        
         public void Remove(T item)
         {
             //Removing an item is a more complicated, because the indices for all items
@@ -49,13 +61,43 @@ namespace CustomListProject
             //into account. This is not completely necessary, but is good for consistency.
             //If capacity is greater than 4 and if the count (after removal) is 1/2 the capacity, then
             //the new array should be 1/2 the current size.
+
+            count--;
+            
         }
 
-        public void Zip(List<T> list)
+        public void Zip(CustomList<T> list)
         {
+            capacity = list.Capacity + capacity;
+            T[] result = new T[capacity];
+            count = list.Count + count;
+            for (int i = 0; i < count; i++)
+            {
+                result[i]=items[2*i];
+                result[i + 1] = items[2 * i + 1];
+            }
+            items = result;
 
         }
 
+        //Below is my attempt at an indexer. Not sure if
+        //correct, but will work temporarily as placeholder.
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
 
+        //
+        public override string ToString()
+        {
+            
+        }
     }
 }
